4c9d4c8304855b4725d7ea5b1bec19f123f32864
Zhang, Ruiqi <ruiqzhang@paypal.com>
2017-09-05 17:07:04 +0800

    refine project dist info & pytest proceduresπ

--- setup.py
+++ setup.py
@@ -1,19 +1,42 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 from querify import VERSION
 
 
 setup(
     name='querify',
-    packages=['querify'],
     version=VERSION,
     description='InfluxQL / MySQL / Mongo query generator from json-represented filters (mongodb-like query objects)',
     author='Raychee Zhang',
     author_email='raychee.zhang@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Framework :: Pytest',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+    keywords=['json', 'mongo', 'influx', 'mysql', 'query'],
     url='https://github.com/Raychee/querify',
     download_url='https://github.com/Raychee/querify/tarball/' + VERSION,
-    keywords=['json', 'mongo', 'influx', 'mysql'],
+    packages=find_packages(exclude=['*.test']),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     install_requires=[
         'qutils>=0.3.0'
-    ]
+    ],
+    python_requires='>=3',
 )
--- tag_and_upload_to_pypi.sh
+++ tag_and_upload_to_pypi.sh
@@ -8,4 +8,5 @@ COMMENT=$(git log -1 --pretty=%B | cat)
 
 git tag ${VERSION} -m "${COMMENT}"
 git push origin --tags && \
-python setup.py sdist upload -r pypi
+python setup.py sdist bdist_wheel
+twine upload --skip-existing dist/*
